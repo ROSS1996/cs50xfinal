69d5a8ab9416221d1dc04457cac9176a824642bd
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-02-10 11:02:48 -0300

    Update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,49 @@
 import { type NextPage } from "next";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/layout";
+import teams from "../../public/data/teams.json";
+
+interface Team {
+  name: string;
+  players: string[];
+}
+
+const Teamlist: React.FC = () => {
+  const [teamsState, setTeamsState] = useState<Team[]>([]);
+
+  useEffect(() => {
+    setTeamsState(teams.teams);
+  }, []);
+
+  return (
+    <div>
+      {teamsState.map((team, index) => (
+        <div
+          key={index}
+          className="grid grid-cols-[100px,1fr] grid-rows-1 gap-2 border"
+        >
+          <h1 className="h-full bg-gray-400 p-4 text-center font-bold">
+            {team.name}
+          </h1>
+          <ul className="grid grid-cols-5 items-center p-4">
+            {team.players.map((player, playerIndex) => (
+              <li key={playerIndex} className="font-bold">
+                {player}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   return (
     <Layout title="Home">
-        <section className="flex flex-row flex-wrap gap-4 p-2">
-          <ul className="flex flex-col gap-2 border p-2">
-            <h1 className="font-bold text-center">Furia</h1>
-            <li>Art</li>
-            <li>KSCERATO</li>
-            <li>Yuurih</li>
-            <li>Drop</li>
-            <li>Saffee</li>
-          </ul>
-          <ul className="flex flex-col gap-2 border p-2">
-            <h1 className="font-bold text-center">Imperial</h1>
-            <li>Fallen</li>
-            <li>Boltz</li>
-            <li>Chelo</li>
-            <li>Jota</li>
-            <li>VINI</li>
-          </ul>
-          <ul className="flex flex-col gap-2 border p-2">
-            <h1 className="font-bold text-center">00Nation</h1>
-            <li>Coldzera</li>
-            <li>Taco</li>
-            <li>Dumau</li>
-            <li>Latto</li>
-            <li>nqz</li>
-          </ul>
-          <ul className="flex flex-col gap-2 border p-2">
-            <h1 className="font-bold text-center">MIBR</h1>
-            <li>Exit</li>
-            <li>Tuurtle</li>
-            <li>HEN1</li>
-            <li>Insani</li>
-            <li>Brnzan</li>
-          </ul>
-          <ul className="flex flex-col gap-2 border p-2">
-            <h1 className="font-bold text-center">PaiN</h1>
-            <li>PKL</li>
-            <li>Biguzera</li>
-            <li>Hardzao</li>
-            <li>Skullz</li>
-            <li>Zevy</li>
-          </ul>
-          <ul className="flex flex-col gap-2 border p-2">
-            <h1 className="font-bold text-center">Fluxo</h1>
-            <li>Woody</li>
-            <li>VSM</li>
-            <li>Lucaozy</li>
-            <li>Felps</li>
-            <li>History</li>
-          </ul>
-          <ul className="flex flex-col gap-2 border p-2">
-            <h1 className="font-bold text-center">O PLANO</h1>
-            <li>KNG</li>
-            <li>Shz</li>
-            <li>Decenty</li>
-            <li>Piria</li>
-            <li>Lub</li>
-          </ul>
-        </section>
+      <section className="flex flex-col gap-4 p-2">
+        <Teamlist />
+      </section>
     </Layout>
   );
 };
