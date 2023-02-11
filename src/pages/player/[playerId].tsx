3d4f4bbeb6916cbc28210964a691daa6a67edd30
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-02-11 11:41:08 -0300

    Update player page

--- src/pages/player/[playerId].tsx
+++ src/pages/player/[playerId].tsx
@@ -22,7 +22,18 @@ const PlayerPage = () => {
 
   return (
     <Layout title="Home">
-      <h1>{player?.name}</h1>
+      <section className="grid gap-2 p-2">
+        <div className="grid min-w-min max-w-fit grid-cols-2 border border-black bg-green-50">
+          <div className="border-r border-black">
+            <img src={player?.picture} alt="" className="h-auto w-56" />
+          </div>
+          <div className="p-2">
+            <h1 className="font-bold">{player?.name}</h1>
+            <p>{player?.team}</p>
+            <p>Função: {player?.function}</p>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
