03529d4fb224220feed39b23febd55d1384e60cb
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-02-13 07:53:55 -0300

    Update player data, add footer, modify header

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,18 @@ import type { NextPage } from "next";
 
 const Footer: NextPage = () => {
   return (
-    <footer className="hidden">
-      <h1>Footer</h1>
+    <footer className="flex items-center justify-center bg-green-800 text-sm text-white">
+      <p>
+        Desenvolvido por{" "}
+        <a
+          href="https://github.com/ROSS1996/"
+          target="_blank"
+          rel="noreferer noreferrer"
+          className="underline underline-offset-2"
+        >
+          Robson Santana
+        </a>
+      </p>
     </footer>
   );
 };
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ const Header: NextPage = () => {
   return (
     <header>
       <nav>
-        <ul className="flex flex-row items-center gap-4 bg-green-800 p-2 font-bold text-white">
+        <ul className="flex flex-row items-center gap-4 border-b bg-green-800 p-2 font-bold text-white">
           <Link href={"/"}>
             <li className="flex flex-row items-center gap-1">
               <RiTeamFill />
@@ -21,6 +21,20 @@ const Header: NextPage = () => {
           </Link>
         </ul>
       </nav>
+      <div className="flex items-center justify-center bg-green-700 py-1">
+        <span className="borde px-2 text-sm text-white">
+          Dados Retirados de{" "}
+          <a
+            href="https://hltv.org"
+            target="_blank"
+            rel="noreferer noreferrer"
+            className="hover:underline"
+          >
+            HLTV.org
+          </a>{" "}
+          | Intervalo dos Dados: 01/08/2022 - 13/02/2023
+        </span>
+      </div>
     </header>
   );
 };
